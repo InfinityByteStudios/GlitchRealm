c5ec003534c90dc198dbaff3b40fa895f3bdcc12
InfinityByte <infinitybyte.dev@gmail.com>
2025-12-18 10:00:34 -0700

    Deleted Game folder

--- Games/CodeRunner/test_player.js
+++
@@ -1,8 +0,0 @@
-// Simple test to check if Player class works
-import { Player } from './src/core/player.js';
-
-try {
-    const player = new Player(100, 100);
-    } catch (error) {
-    console.error('Error loading Player class:', error);
-}
